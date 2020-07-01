e075cb2e86d3702ceb7b0eefaa619b715c4aca18
jacobwicks <jacob.m.wicks@gmail.com>
2020-07-01 02:04:49 -0700

    Updates

---
+++ precache-manifest.564657f87a0471bbd8f51da92b71779e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f845bbee873bb8f1623f13d9172b801",
+    "url": "/forumsBotInstructions/index.html"
+  },
+  {
+    "revision": "570abcc5193d84b06ec0",
+    "url": "/forumsBotInstructions/static/css/main.c5d8ebdf.chunk.css"
+  },
+  {
+    "revision": "bc82115053852d653131",
+    "url": "/forumsBotInstructions/static/js/2.2952a1d5.chunk.js"
+  },
+  {
+    "revision": "f0ab5801985173a52868f196793396c8",
+    "url": "/forumsBotInstructions/static/js/2.2952a1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "570abcc5193d84b06ec0",
+    "url": "/forumsBotInstructions/static/js/main.68a926ba.chunk.js"
+  },
+  {
+    "revision": "eb0653472b3d9e14d725",
+    "url": "/forumsBotInstructions/static/js/runtime-main.92360dd7.js"
+  }
+]);
