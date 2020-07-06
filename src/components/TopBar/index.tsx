7c63bba284847a43fecda910a19c10b88c1da333
jacobwicks <jacob.m.wicks@gmail.com>
2020-07-06 15:40:35 -0700

    fixed err

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -8,10 +8,15 @@ const Title = () => {
     return <Header size="large" content={`${botName}, an SA Forums Bot`} />;
 };
 
-const InstructionsLink = () => <a href={'https://jacobwicks.github.io/2020/07/01/forums-posting-bot.html'} target="_blank" rel="noopener noreferrer">
-            How to run your own forums bot
-        </a>
-};
+const InstructionsLink = () => (
+    <a
+        href={'https://jacobwicks.github.io/2020/07/01/forums-posting-bot.html'}
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        How to run your own forums bot
+    </a>
+);
 
 const menuItems = [<Title />].map((element, index) => (
     <Menu.Item key={index}>{element}</Menu.Item>
